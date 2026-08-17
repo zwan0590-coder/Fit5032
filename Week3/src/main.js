b83d8bb267ed702465091c720dfe09e8d3189616
zwan0590 <zwan0590@student.monash.edu>
2026-08-17 18:56:44 +1000

    Fix Week3 folder contents

---
+++ Week3/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import './style.css'
+
+createApp(App).mount('#app')
